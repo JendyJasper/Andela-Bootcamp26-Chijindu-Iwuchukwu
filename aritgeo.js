1a1a7d172e36ca6a18a546deb500c2b15d047621
Jendy Jasper <Jendy36@gmail.com>
2017-08-28 16:36:02 +0100

    add a function that checks if items in an array is arithmetic, geometric, or empty or if it is none of the above

---
+++ aritgeo.js
@@ -0,0 +1,58 @@
+
+
+// function returns true if array is an Arithmetic Progression
+const arithmetic = (array) => {
+    let commonDiff = array[1] - array[0];
+    for (let i = 0; i < array.length -1; i++) {
+        if (commonDiff !== (array[i+1] - array[i])) {
+            return false;
+        }
+    }
+    return true;	
+
+}
+
+// function returns true if array is a Gometric Progression
+const geometric = (array) => {
+    let commonRatio = array[1] / array[0];
+    for (let i = 0; i < array.length - 1; i++){
+        if (commonRatio !== (array[i+1] / array[i])) {
+            return false; 
+        }
+    }
+    return true;
+}
+
+// function checks if it's an array and is empty
+const emptyArray = (array) => {
+    if ((array.constructor === Array ) && (array.length < 1 )){
+        return true;
+    }
+    return false;
+}
+
+
+
+
+const aritGeo = (array) => {
+        //return 0 if an empty array
+        if (emptyArray(array)) {
+            return 0;		
+        }
+        //return Arithmetic for arithmetric progression
+        if (arithmetic(array)) {
+            return "Arithmetic";
+        }
+        // return Geometric if geometric progression
+        if (geometric(array)) {
+            return "Geometric";
+        }
+        //return -1 for neither
+        return -1; 
+    }
+
+//Test
+//console.log(aritGeo([2, 4, 8, 16])) //for Geometric test
+//console.log(aritGeo([2, 4, 6, 8])) //for Arithmetic test
+//console.log(aritGeo([])) //for empty array test
+//console.log(aritGeo([2, 9, 8, 1])) //for neither arithmetic, geometric, nor empt array test
